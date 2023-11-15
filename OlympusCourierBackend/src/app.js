980c990eea6ee95b020561f3e13ebcab8f214dbd
AppDevSYS <106793712+JoelMaldonado@users.noreply.github.com>
2023-11-15 15:18:35 -0500

    d

--- OlympusCourierBackend/src/app.js
+++ OlympusCourierBackend/src/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const db = require('./mysql.js');
+const { db } = require('./mysql.js');
 const dotenv = require('dotenv');
 
 const destinoRoutes = require('./routes/destino.routes.js');
